6542fb04218efb1df1d7c24ce90af079811cf010
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2015-12-07 21:41:21 -0200

    Created uri_util.py.

---
+++ uri_util.py
@@ -0,0 +1,21 @@
+import os
+
+
+def get_file_from_path(path):
+    source_code = ""
+    if not os.path.isfile(path):
+        return source_code
+    with open(path) as f:
+        source_code = f.read()
+    return source_code
+
+
+def get_extension_from_path(path):
+    if not os.path.isfile(path):
+        return None
+    extension = os.path.splitext(path)[1]
+    return extension[1:]
+
+
+def get_home_path():
+    return os.path.expanduser('~')
