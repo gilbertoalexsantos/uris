9daef20a0a7394eea49c100f98608089c9c818cc
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2015-12-15 17:14:41 -0200

    Created command helper.

---
+++ uri/commands/helper.py
@@ -0,0 +1,6 @@
+from .submission import brief_description as submission_brief_description
+
+
+def execute_helper_command(flags):
+    print "Command to get last submissions:"
+    print "  ", submission_brief_description
