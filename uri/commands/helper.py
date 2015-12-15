64b5d4cf498760c9f7e4c7a1fe75442dfe473933
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2015-12-15 18:07:29 -0200

    Updated helper.py.

--- uri/commands/helper.py
+++ uri/commands/helper.py
@@ -1,6 +1,22 @@
 from .submission import brief_description as submission_brief_description
+from .submit import brief_description as submit_brief_description
+from .setup import brief_description as setup_brief_description
+
+
+brief_description = """\
+uri help\
+"""
 
 
 def execute_helper_command(flags):
-    print "Command to get last submissions:"
+    print "Last submissions:"
     print "  ", submission_brief_description
+
+    print "Submit a problem:"
+    print "  ", submit_brief_description
+
+    print "Setup settings:"
+    print "  ", setup_brief_description
+
+    print "Helper:"
+    print "  ", brief_description
