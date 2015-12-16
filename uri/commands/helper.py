6756c083b0e102ff5666e6f7f2adeb111f282e79
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2015-12-16 00:54:34 -0200

    Refactored helper.py and added last.

--- uri/commands/helper.py
+++ uri/commands/helper.py
@@ -1,22 +1,39 @@
 from .submission import brief_description as submission_brief_description
 from .submit import brief_description as submit_brief_description
 from .setup import brief_description as setup_brief_description
+from .last_submission import brief_description as last_submission_brieft_description
 
 
 brief_description = """\
 uri help\
 """
 
+help_description = """\
+If you want detailed explanation about a command, use the --help flag.
+
+Example:
+  uri subs --help
+  uri last --help
+
+Commands:
+  Submissions
+    {submissions}
+  Last submission
+    {last_submission}
+  Submit a problem
+    {submit}
+  Setup settings
+    {setup}
+  Helper
+    {helper}\
+""".format(
+    submissions=submission_brief_description,
+    last_submission=last_submission_brieft_description,
+    submit=submit_brief_description,
+    setup=setup_brief_description,
+    helper=brief_description
+)
+
 
 def execute_helper_command(flags):
-    print "Last submissions:"
-    print "  ", submission_brief_description
-
-    print "Submit a problem:"
-    print "  ", submit_brief_description
-
-    print "Setup settings:"
-    print "  ", setup_brief_description
-
-    print "Helper:"
-    print "  ", brief_description
+    print help_description
