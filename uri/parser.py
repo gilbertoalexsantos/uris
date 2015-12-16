fec1f5299ca5383a544d95146b829f6a6d47295f
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2015-12-16 00:52:40 -0200

    Parser now stores other arguments in a key 'others'.

--- uri/parser.py
+++ uri/parser.py
@@ -10,7 +10,10 @@ def parser_args(args):
     if not args:
         return {}
     
-    flags = {'command': args[0]}
+    flags = {
+        'command': args[0],
+        'others': []
+    }
 
     for arg in args[1:]:
         match_value = FLAG_VALUE_REGEX.search(arg)
@@ -19,5 +22,8 @@ def parser_args(args):
             flags[match_value.group('flag')] = match_value.group('value')
         elif match_alone:
             flags[match_alone.group('flag')] = True
+        else:
+            flags['others'].append(arg)
+
 
     return flags
